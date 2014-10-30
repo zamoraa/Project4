8ead92c4748dd67821c9e8eb36e45bdd48658851
Your Name <azjr112@sbcglobal.net>
2014-10-30 09:44:19 -0700

    Added a comment

--- hello.cpp
+++ hello.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 int main()
 {
-//Default  is "World"
+#Default is "World"
 name = ARGV.first || "World"
+
 outs << "Hello, #{name}!" 
 //outs << "Hello, #{ARGV.first}!" << endl;
 }
