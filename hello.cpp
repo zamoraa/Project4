01c4509a4958c11ba2b77dca023a84cc91f9c1bf
Alvaro Zamora <azjr112@sbcglobal.net>
2014-10-30 09:27:33 -0700

    Using Argv

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
+
 #inlucde <iostream>
 using namespace std;
 int main()
 {
 //Default is "World"
 name = ARGV.first || "World"
-outs << "Hello, #{name}!" 
+outs << "Hello, #{ARGV.first}!" 
 //outs << "Hello, #{ARGV.first}!" << endl;
 }
